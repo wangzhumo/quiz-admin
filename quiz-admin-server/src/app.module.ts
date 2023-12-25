90134110ede0260f2306f9f5a8332c5b436beebc
wangzhumo <wangzhumoo@gmail.com>
2023-12-25 19:56:20 +0800

    [feature]ignore

--- quiz-admin-server/src/app.module.ts
+++ quiz-admin-server/src/app.module.ts
@@ -3,14 +3,24 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ImagesController } from './modules/images/images.controller'
 import { ImagesModule } from './modules/images/images.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import Configuration from './configuration'
+import { QiniuModule } from 'modules/qiniu/qiniu.module'
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
             load: [Configuration],
+        }),
+        QiniuModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    sk: configService.get<string>('qiniu.sk'),
+                    ak: configService.get<string>('qiniu.ak'),
+                }
+            },
         }),
         ImagesModule,
     ],
