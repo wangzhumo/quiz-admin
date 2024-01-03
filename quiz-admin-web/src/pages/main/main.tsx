0b338e1abb7ffaee328b381e6a7a04a4e82ce98c
wangzhumo <wangzhumoo@gmail.com>
2024-01-03 23:55:32 +0800

    [feature]Persistent drawer

--- quiz-admin-web/src/pages/main/main.tsx
+++ quiz-admin-web/src/pages/main/main.tsx
@@ -1,30 +1,139 @@
 import SliderComponent from '@/components/slider/slider'
-import { Outlet } from 'react-router-dom'
-import { Layout } from 'antd'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import { Box } from "@mui/material";
+import { Box } from '@mui/material'
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Typography from '@mui/material/Typography'
+import { styled } from '@mui/material/styles'
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 
-const { Sider, Content } = Layout
+const drawerWidth = 240
+
+const Main = styled('main', { shouldForwardProp: props => props !== 'open' })<{
+  open?: boolean
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  marginLeft: `-${drawerWidth}`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}))
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: props => props !== 'open'
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}))
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end'
+}))
 
 const MainLayout = () => {
+  const theme = useTheme()
+  const [open, setOpen] = useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
   return (
-    <Box>
-      <Sider
-        className={'app-sidebar'}
-        collapsible={true}
-        defaultCollapsed={false}
-        reverseArrow={true}
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position='fixed' open={open}>
+        <div>AppBar AppBar AppBar AppBar AppBar</div>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box'
+          }
+        }}
+        variant='persistent'
+        anchor='left'
+        open={open}
       >
-        <SliderComponent />
-      </Sider>
-      <Box className='app-main'>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            <div>Close</div>
+          </IconButton>
+        </DrawerHeader>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <div>Button</div>
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <div>Button</div>
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
         <Outlet />
-      </Box>
+      </Main>
     </Box>
   )
 }
 
 export default MainLayout
+function useTheme() {
+  throw new Error('Function not implemented.')
+}
